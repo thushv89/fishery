6243d1a6e20b5dd0aa829db146f63165e665e723
Local Admin <Local Admin>
2016-11-21 17:20:50 +1100

    fixed issues in load_data. load_data couldnt find enough memoery. so I am saaving data in all folders separately first to pickles

--- load_data.py
+++ load_data.py
@@ -6,31 +6,36 @@ import numpy as np
 import os
 from scipy import ndimage
 from math import floor,ceil
+from skimage.measure import block_reduce
+from scipy.misc import imsave
 
 image_width,image_height,num_channels = 1280,720,3
 
-def load_fishery_data():
+def load_fishery_data(resize=True,test_image=False,test_pickle=False):
 
     augment_data = False
 
     fishery_train_dir = 'train'
-    fishery_test_dir = 'test_sg1'
-    cropped_width,cropped_height = 670,670 #min width 1192, min height 670
+    fishery_test_dir = 'test_stg1'
+    cropped_width,cropped_height = 640,640 #min width 1192, min height 670
     labels = ['ALB','BET','DOL','LAG','NoF','OTHER','SHARK','YFT']
 
     valid_size = 2000
     if not os.path.exists(fishery_train_dir):
         raise FileNotFoundError
 
-    img_dataset = []
-    img_labels = []
-    pixel_depth = -1
 
     min_width,min_height = 5000,5000
     find_min = False
 
     for l_i,lbl in enumerate(labels):
+        if test_pickle and l_i==0:
+            continue
+
         print("Processing folder %s ..."%lbl)
+        img_dataset = []
+        img_labels = []
+        pixel_depth = -1
         for file in os.listdir(fishery_train_dir+os.sep+lbl):
             if file.endswith(".jpg"):
                 image_data = ndimage.imread(fishery_train_dir+os.sep+lbl+os.sep+file).astype(float)
@@ -51,81 +56,71 @@ def load_fishery_data():
                         image_data = np.delete(image_data,np.r_[image_data.shape[0]-cut_from_bottom:image_data.shape[0]],axis=0)
 
                     assert image_data.shape == (cropped_height,cropped_width,num_channels)
-                    image_data = (image_data - (pixel_depth/2.0))/pixel_depth
+                    image_data = (image_data - (pixel_depth/2.0))/(pixel_depth/2.0)
+
+                    if resize:
+                        image_data = block_reduce(image_data,(2,2,1))
+
                     img_dataset.append(image_data)
                     img_labels.append(l_i)
+
+                    if test_image:
+                        imsave('test_'+lbl+'.png', image_data)
+                        break
+
                 else:
                     if image_data.shape[1]<min_width:
                         min_width = image_data.shape[1]
                     if image_data.shape[0]<min_height:
                         min_height = image_data.shape[0]
+        try:
+            with open('fishery_'+lbl+'.pickle', 'wb') as f:
+                img_dataset = np.asarray(img_dataset)
+                img_labels = np.asarray(img_labels)
+                print("Dataset shape %s"%str(img_dataset.shape))
+                print("Labels shape %s"%str(img_labels.shape))
+                pickle.dump({'dataset':image_data,'labels':img_labels}, f, pickle.HIGHEST_PROTOCOL)
+        except Exception as e:
+            print('Unable to save fishery_data (%s): %s' %(lbl,str(e)))
 
-    print("Min width: %d min height: %d"%(min_width,min_height))
-    img_dataset = np.asarray(img_dataset)
-    img_labels = np.asarray(img_labels)
-    print("Dataset shape %s"%img_dataset.shape)
-    print("Labels shape %s",img_labels.shape)
-
-    if augment_data:
-        for pmu in range(img_dataset.shape[0]):
-            img_dataset = np.append(img_dataset,np.fliplr(img_dataset[pmu,:,:,:]),axis=0)
-            img_labels = np.append(img_labels,img_labels[pmu],axis=0)
-            img_dataset = np.append(img_dataset,np.flipud(img_dataset[pmu,:,:,:]),axis=0)
-            img_labels = np.append(img_labels,img_labels[pmu],axis=0)
-            img_dataset = np.append(img_dataset,ndimage.rotate(img_dataset[pmu,:,:,:]), np.random.randint(-45,45), reshape= False)
-            img_labels = np.append(img_labels,img_labels[pmu],axis=0)
-
-    img_perm = np.random.permutation(img_dataset.shape[0])
-    valid_start_idx = np.random.randint(0,img_dataset.shape[0]-valid_size)
-    valid_dataset = img_dataset[img_perm[valid_start_idx:valid_start_idx+valid_size],:,:,:]
-    valid_labels = img_labels[img_perm[valid_start_idx:valid_start_idx+valid_size],:,:,:]
-
-    print("Valid dataset shape %s"%valid_dataset.shape)
-    print("Valid labels shape %s"%valid_labels.shape)
-
-    train_dataset = np.delete(img_dataset,np.r_[img_perm[valid_start_idx:valid_start_idx+valid_size]])
-    train_labels = np.delete(img_labels,np.r_[img_perm[valid_start_idx:valid_start_idx+valid_size]])
-
-    print("Test dataset shape %s"%train_dataset.shape)
-    print("Test labels shape %s"%train_labels.shape)
+        if test_pickle:
+            break
 
     print('Composing test data ...')
 
+    del img_dataset
+    del img_labels
     test_dataset = []
     test_labels = []
-    for file in os.listdir(fishery_test_dir):
-        if file.endswith(".jpg"):
-            image_data = ndimage.imread(file).astype(float)
-            assert image_data.shape == (image_width,image_height,num_channels)
-            # crop from right
-            image_data = np.delete(image_data,np.r_[0:(image_width-cropped_width)//2],axis=1)
-            # crop from left
-            image_data = np.delete(image_data,np.r_[-(image_width-cropped_width)//2:image_data.shape[1]],axis=1)
+    if not test_image and not test_pickle:
+        for file in os.listdir(fishery_test_dir):
+            if file.endswith(".jpg"):
+                image_data = ndimage.imread(file).astype(float)
+                assert image_data.shape == (image_width,image_height,num_channels)
+                # crop from right
+                image_data = np.delete(image_data,np.r_[0:(image_width-cropped_width)//2],axis=1)
+                # crop from left
+                image_data = np.delete(image_data,np.r_[-(image_width-cropped_width)//2:image_data.shape[1]],axis=1)
 
-            image_data = (image_data - (pixel_depth/2.0))/pixel_depth
-            if image_height != cropped_height:
-                raise NotImplementedError
+                image_data = (image_data - (pixel_depth/2.0))/(pixel_depth/2.0)
+                if image_height != cropped_height:
+                    raise NotImplementedError
 
-            test_dataset.append(image_data)
-            test_labels.append(l_i)
+                test_dataset.append(image_data)
+                test_labels.append(l_i)
 
-    test_dataset = np.asarray(test_dataset)
-    test_labels = np.asarray(test_labels)
+        test_dataset = np.asarray(test_dataset)
+        test_labels = np.asarray(test_labels)
 
-    print("Test dataset shape %s"%test_dataset.shape)
-    print("Test labels shape %s"%test_labels.shape)
+        print("Test dataset shape %s"%test_dataset.shape)
+        print("Test labels shape %s"%test_labels.shape)
 
-    print('Successfully whitened data ...\n')
-    print('\nDumping processed data')
-    fishery_data = {'train_dataset':train_dataset,'train_labels':train_labels,
-                  'valid_dataset':valid_dataset,'valid_labels':valid_labels,
-                  'test_dataset':test_dataset,'test_labels':test_labels
-                  }
-    try:
-        with open('fishery.pickle', 'wb') as f:
-            pickle.dump(fishery_data, f, pickle.HIGHEST_PROTOCOL)
-    except Exception as e:
-        print('Unable to save fishery_data:', e)
+        print('Successfully whitened data ...\n')
+        try:
+            with open('fishery-test-stg1.pickle', 'wb') as f:
+                pickle.dump({'dataset':test_dataset,'labels':test_labels}, f, pickle.HIGHEST_PROTOCOL)
+        except Exception as e:
+            print('Unable to save fishery_data:', e)
 
 
 def reformat_data_fishery():
